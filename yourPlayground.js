0278c76029e05eaa6fa1c19329c15b670ba41705
NABIL ADIB <nabil298adib@gmail.com>
2022-07-06 20:43:03 +0800

    ADD:letter frequency project
    TEST:for loops and if else statement,object adding,accesing key to get value and increment it from the key

--- yourPlayground.js
+++ yourPlayground.js
@@ -33,3 +33,25 @@ const maxNumber = (numbers) =>{
 }
 const numbers =[1,2,3,4,5,6,2,3]
 maxNumber(numbers)
+
+//PROJECT:Calculate frequencies of anything
+//ALGO:Expected to return object that show key=letter,value=number of times appeared
+//TODO: Loop through the phrase
+//TODO:Create empty object 
+//TODO:Check if the letter exists in the frequency 
+//TODO:if exists,increment it
+//TODO:otherwise, set the value to 1
+
+const letterFrequency =(phrases) =>{
+  let frequency ={};
+  for(letter of phrases){
+    if(letter in frequency){
+      //This is usefull for adding element within loop because most loop return string
+    frequency[letter] = frequency[letter] + 1}
+    else{
+      frequency[letter] = 1
+    }
+  }
+  return frequency
+}
+console.log(letterFrequency("haha"))
